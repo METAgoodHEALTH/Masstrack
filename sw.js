47850342245d57e7bb185ecee7b6b81cdfed2ba4
METAgoodHEALTH <majd-el-mokhtar.el-mehri@eleves.enpc.fr>
2026-09-06 14:22:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,25 @@
 
-const CACHE="masstrack-android-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="masstrack-android-v8-achats";
+const STATIC=["./manifest.webmanifest"];
+self.addEventListener("install",event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+self.addEventListener("fetch",event=>{
+  const req=event.request;
+  if(req.mode==="navigate" || new URL(req.url).pathname.endsWith("/index.html")){
+    event.respondWith(fetch(req).then(res=>{
+      const copy=res.clone(); caches.open(CACHE).then(c=>c.put(req,copy)); return res;
+    }).catch(()=>caches.match(req)));
+    return;
+  }
+  event.respondWith(caches.match(req).then(cached=>cached || fetch(req).then(res=>{
+    const copy=res.clone(); caches.open(CACHE).then(c=>c.put(req,copy)); return res;
+  })));
+});
