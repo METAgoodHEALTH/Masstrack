474094ae9278bfa9575efa599fc0e77d976ccf10
METAgoodHEALTH <majd-el-mokhtar.el-mehri@eleves.enpc.fr>
2026-09-14 23:37:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,5 @@
-
-const CACHE="masstrack-android-v8-achats";
+const CACHE="masstrack-android-v10-calendar";
 const STATIC=["./manifest.webmanifest"];
-self.addEventListener("install",event=>{
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)));
-});
-self.addEventListener("activate",event=>{
-  event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
-self.addEventListener("fetch",event=>{
-  const req=event.request;
-  if(req.mode==="navigate" || new URL(req.url).pathname.endsWith("/index.html")){
-    event.respondWith(fetch(req).then(res=>{
-      const copy=res.clone(); caches.open(CACHE).then(c=>c.put(req,copy)); return res;
-    }).catch(()=>caches.match(req)));
-    return;
-  }
-  event.respondWith(caches.match(req).then(cached=>cached || fetch(req).then(res=>{
-    const copy=res.clone(); caches.open(CACHE).then(c=>c.put(req,copy)); return res;
-  })));
-});
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{const r=e.request;if(r.mode==="navigate"||new URL(r.url).pathname.endsWith("/index.html")){e.respondWith(fetch(r).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put(r,cp));return res}).catch(()=>caches.match(r)));return;}e.respondWith(caches.match(r).then(x=>x||fetch(r).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put(r,cp));return res})));});
